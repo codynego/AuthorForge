cf8b693485353e9ad2e8f393e7665cbf27b6f4ea
codynego <emonenaabednego@gmail.com>
2023-02-23 22:26:18 +0500

    created book, genre and tone class to the app model

--- bookcreator/models.py
+++ bookcreator/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Tone(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
