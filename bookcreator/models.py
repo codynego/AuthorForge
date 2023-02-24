5142f2120893fa4c76e917417adfee80df4b78b1
codynego <emonenaabednego@gmail.com>
2023-02-24 13:39:29 +0500

    added some model classes

--- bookcreator/models.py
+++ bookcreator/models.py
@@ -3,22 +3,49 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
 
+class Chapter(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+class SubChapter(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 class Genre(models.Model):
     name = models.CharField(max_length=100)
-    content = models.TextField()
 
     def __str__(self):
         return self.name
 
 class Tone(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class GenrePrompt(models.Model):
     name = models.CharField(max_length=100)
+    prompt = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class GenreContentPrompt(models.Model):
+    name = models.CharField(max_length=100)
+    prompt = models.TextField()
 
     def __str__(self):
         return self.name
