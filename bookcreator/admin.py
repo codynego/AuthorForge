939a59e3b7db991ca1aa387e4bbe924bf072b897
codynego <emonenaabednego@gmail.com>
2023-02-24 13:33:54 +0500

    registered the models to django admin

--- bookcreator/admin.py
+++ bookcreator/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book, Chapter, SubChapter, Genre, Tone, GenrePrompt, GenreContentPrompt
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Chapter)
+admin.site.register(SubChapter)
+admin.site.register(Genre)
+admin.site.register(Tone)
+admin.site.register(GenrePrompt)
+admin.site.register(GenreContentPrompt)
